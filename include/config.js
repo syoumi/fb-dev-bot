5d84a5798b79439fdbb64ada558ef71d88150b65
Mahdi <Mahdi.syoumi@gmail.com>
2017-07-26 18:14:14 +0100

    Fixing bugs

--- include/config.js
+++ include/config.js
@@ -2,7 +2,7 @@
 const PORT = process.env.PORT || 9191;
 const FB_PAGE_TOKEN = 'EAAZAXZAcQxCAsBABD7nCVby8n7eI5NjzDDNYCNNnXrKZBsL0nAaLmA28PzjRLovOL7B5QyupRaFFbMTirZAtccJIdpTVaDThieg2TjNoMwGBU41ZBw5NgmbsyR3CgQrki1Am1eF167NY7Q5phV4YtIQHTeVuhEgTn60zv5ZCoxLtYbO3EoZCtoB';
 const VERIFY_TOKEN = 'SIMPLE_VERIFY_TOKEN';
-const WAITING_BEFORE_PROMPT = 5000; // 5 secondes
+const WAITING_BEFORE_PROMPT = 10000; // 5 secondes
 
 var waitingForAnswer = false;
 
