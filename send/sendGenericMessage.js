5e68b3d76ad38b95a52ba0520b6bc81297e3c94f
Mahdi <Mahdi.syoumi@gmail.com>
2017-07-26 13:49:40 +0100

    Fixing logs

--- send/sendGenericMessage.js
+++ send/sendGenericMessage.js
@@ -5,6 +5,7 @@ const {callSendAPI} = require('./sendViaFaceBookAPI');
 
 var sendGenericMessage = () => (reciepientID) => {
   // on structure notre message, préparation pour l'envoi à l'API
+  console.log(reciepientID);
   var messageData = {
     recipient: {
       id: reciepientID
