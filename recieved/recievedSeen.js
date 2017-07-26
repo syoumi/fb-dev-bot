5635d4e0e788c54161797124217e6725813a3608
Mahdi <Mahdi.syoumi@gmail.com>
2017-07-26 17:13:22 +0100

    Fixing bugs

--- recieved/recievedSeen.js
+++ recieved/recievedSeen.js
@@ -6,6 +6,7 @@ var recievedSeen = (event) => {
   if (waitingForAnswer) {
     setTimeout(() => {
       if (waitingForAnswer) {
+        console.log('Sending confirmation to user');
         sendTextMessage(event.sender.id, 'J\'attends toujours votre réponse !!');
         waitingForAnswer = false;
       }
