43cf100402f53335ee48a279dc835d5963777e7c
Mahdi <Mahdi.syoumi@gmail.com>
2017-07-26 14:17:14 +0100

    Edit postbacks

---
+++ recieved/recievedPostBack.js
@@ -0,0 +1,21 @@
+// Ce fichier implémente la logique de la manipulation des postbacks
+
+const {sendTextMessage} = require('./../send/sendTextMessage');
+
+var recievedPostBack = (event) => {
+  // On commence toujours par avoir les coordonnées/informations
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostBack = event.timestamp;
+
+  // On récupère le payload (Qu'on défini lors de la création du bouton)
+  // pour savoir quelle action à executer
+  var payload = event.postback.payload;
+
+  // On se contente ici uniquement de logger les informations et envoyer du texte
+  console.log(`### Postback recieved informations ###`);
+  console.log(`senderID ${senderID}, recipientID ${recipientID}, time ${timeOfPostBack}`);
+  console.log('#################END PRI##############');
+
+  sendTextMessage(senderID, 'Postback recu avec succes');
+};
