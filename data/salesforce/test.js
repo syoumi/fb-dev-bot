8a6342a407c754eaedae3bd6bbfde6c38ec4c5b7
Mahdi <Mahdi.syoumi@gmail.com>
2017-07-31 09:24:13 +0100

    fixing connexion to sf

--- data/salesforce/test.js
+++ data/salesforce/test.js
@@ -8,7 +8,7 @@ const {SF_PASSWORD} = require('./../../include/config');
 var link = new jsforce.Connection({
   // optionnal
   // We can change the login URL to sandbox or prerelease env.
-  loginUrl: 'https://login.salesforce.com'
+  loginUrl: 'https://test.salesforce.com'
 });
 
 // We make a new connection using our salesforce credentials
