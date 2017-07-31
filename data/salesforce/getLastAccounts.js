201258b8d64808e2e2c73ce6ec3c970025745e3a
Mahdi <Mahdi.syoumi@gmail.com>
2017-07-31 14:07:27 +0100

    fixing bugs

--- data/salesforce/getLastAccounts.js
+++ data/salesforce/getLastAccounts.js
@@ -34,6 +34,7 @@ var getLastAccounts = (senderID) => {
             messagesToSend.push(`The name of the third account is : ${response.records[2].Name}`);
           }
           console.log('Messages to send lenght ' , messagesToSend.length);
+          messagesToSend.forEach((message) => console.log(message));
           sendBulkTextMessages(senderID, messagesToSend);
         });
   });
